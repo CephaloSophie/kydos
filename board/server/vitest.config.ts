f018a8aa95f4633165084159640eb701952727c4
tchernobyl <ameur.hamdouni@gmail.com>
2026-08-06 18:04:32 +0200

    fix the .env issue

--- board/server/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    include: ['src/**/*.test.ts', 'scripts/**/*.test.ts'],
-    reporters: ['default'],
-    testTimeout: 10_000,
-  },
-});
